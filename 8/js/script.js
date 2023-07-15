5d3825104c09123d7307953ccb71feaed7fa5a6e
jafari17 <jafari17@gmail.com>
2023-07-15 16:25:11 +0330

    delete

--- 8/js/script.js
+++
@@ -1,7 +0,0 @@
-const sidebar = document.querySelector('.sidebar');
-const btn = document.querySelector('.btn');
-
-btn.addEventListener('click', function() {
-    sidebar.classList.toggle('active');
-    sidebar.style.visibility = 'visible';
-})
